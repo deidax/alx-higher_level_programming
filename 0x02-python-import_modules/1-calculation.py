b2d6ac5499cb3be6c16c83fef30cbef26c25a46f
deidax <khalildida93@gmail.com>
2023-08-15 02:31:02 -0700

    alx python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
+
 def _cal():
     a = 10
     b = 5
@@ -7,5 +9,7 @@ def _cal():
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+
 if __name__ == "__main__":
     _cal()
