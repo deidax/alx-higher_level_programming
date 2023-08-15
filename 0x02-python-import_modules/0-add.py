6fb8ad73b3c65abdef1738b59aaee112425a17ae
deidax <khalildida93@gmail.com>
2023-08-15 02:26:21 -0700

    alx python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,6 @@ from add_0 import add
 def _add():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 if __name__ == "__main__":
     _add()
