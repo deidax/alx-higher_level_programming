9200d8caf280cbac1a227289fd877f77d13d4aa7
deidax <khalildida93@gmail.com>
2023-08-16 01:48:46 -0700

    alx python

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+s = "{:d} is positive".format(number) if number > 0 else ("{:d} is negative".format(number) if number < 0 else "is zero")
+print(s)
